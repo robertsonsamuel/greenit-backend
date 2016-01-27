5d1c48cfa61b2558dc18eed6800c91283936482a
paulgoblin <mpr75@cornell.edu>
2016-01-26 16:35:03 -0800

    add category and category validation to resource model

--- models/resource.js
+++ models/resource.js
@@ -3,17 +3,23 @@
 const mongoose = require('mongoose')
     , User     = require('./user');
 
+// VALID CATEGORIES
+const VALID_CATEGORIES = new Set(['general', 'javascript', 'ruby', 'html', 'css', 'python', 'go', 'swift']);
+
 let Resource;
 
 let resourceSchema = mongoose.Schema({
   title: { type: String, required: true },
   link: { type: String, required: true},
   body: { type: String, default: "" },
-  user: { type: mongoose.Schema.Types.ObjectId , ref: 'User' },
+  category: { type: String, default: "general" },
+  user: { type: mongoose.Schema.Types.ObjectId , ref: 'User', required: true },
   timestamp: { type : Date, default: Date.now }
 });
 
+
 resourceSchema.statics.createNewResource = (newResource, userId, cb) => {
+  newResource.category = newResource.category.replace(/\W/g, '').toLowerCase();
   newResource.user = userId;
   Resource.create(newResource, (err, savedResource) => {
     return err ? cb(err) : cb(null, savedResource);
@@ -23,13 +29,13 @@ resourceSchema.statics.createNewResource = (newResource, userId, cb) => {
 // VALIDATORS
 resourceSchema.path('user').validate(function (value, respond) {
   User.findById({_id: value}, function (err, foundUser) {
-    if (err || !foundUser) {
-      respond(false);
-    } else {
-      respond(true);
-    }
+    respond( !err && !!foundUser );
   });
-}, 'Error validating user');
+}, 'Error validating resource user');
+
+resourceSchema.path('category').validate(function (value, respond) {
+  respond( VALID_CATEGORIES.has(value) );
+}, 'Error validating resource category');
 
 Resource = mongoose.model('Resource', resourceSchema);
 module.exports = Resource;
